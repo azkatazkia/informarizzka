54f9c648e09fb8a30355da8fb09978aa3d2028bb
ozzzki <145310248+azkatazkia@users.noreply.github.com>
2024-08-29 09:49:05 +0700

    Add files via upload

---
+++ informarizzka.py
@@ -0,0 +1,15 @@
+item = "Mewing"
+Item = "Capt"
+item_name = "Alpha"
+
+print(item, Item)
+print("Show me ur rizz, " + item_name)
+
+integer = 2024
+name = "Kai Cenat"
+string_number = "22"
+isHappy = False
+naughty_list = ["Sigma", "Skibidi", "Toilet"]
+
+print(name + str(integer))
+print(10 + int(string_number))
